77d60a8f452fa3552380c8b2dcd30a33e1fe567c
Danylo Baziewicz <danxbazi@gmail.com>
2025-01-19 14:27:07 -0300

    Ajustando mensagem de alert de acerto.

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 alert('Boas vindas aom jogo do número secreto');
 let numeroSecreto = 5;
 let palpite;
+let tentativas = 1;
 
 while(palpite != numeroSecreto) {
     palpite =  prompt('Escolha um número entre 1 e 30');
     if (numeroSecreto == palpite) {
-        alert('Isso aí! Você acertou o número secreto é: '+numeroSecreto);
+        alert('Isso aí! Você acertou o número secreto é: '+numeroSecreto+ '\n você utilizou '+tentativas+' tentativa(s)');
     } else {
         if (palpite > numeroSecreto) {
             alert('o número secreto é menor que '+palpite);
         } else {
             alert('o número secreto é maior que '+palpite);
         }
+        tentativas++;
     }
 }
